4e54e56d290e03f0f170beab94740297841fcbe5
DeclawedLyon <declan.e.lyons@gmail.com>
2022-05-17 20:04:02 -0700

    refactors polygons to render dynamically using passed down props

--- src/components/maps/MapWindow.jsx
+++ src/components/maps/MapWindow.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { GoogleMap, useJsApiLoader, Polygon } from '@react-google-maps/api'
 
 const apiKey = process.env.REACT_APP_API_KEY;
@@ -17,12 +17,6 @@ const center = {
   lng: -123.41899931709301
 };
 
-const paths = [
-  {lat: 48.43467084543356, lng: -123.41899931709301},
-  {lat: 48.53467084543356, lng: -123.51899931709301},
-  {lat: 48.33467084543356, lng: -123.31899931709301},
-  {lat: 48.43467084543356, lng: -123.41899931709301},
-]
 const options = {
   fillColor: "lightblue",
   fillOpacity: 0.5,
@@ -36,8 +30,25 @@ const options = {
   zIndex: 1
 }
 
+const defaultCoords = () => {
+  return [
+    {lat: 48.43459930069949, lng: -123.41968549014572},
+    {lat: 48.434594894435115, lng: -123.41938888555694},
+    {lat: 48.43454781192849, lng: -123.41933211475094},
+    {lat: 48.434378314543366, lng: -123.41932501840019},
+    {lat: 48.43418056687943, lng: -123.41936759650467},
+    {lat: 48.43378506924258, lng: -123.41850184171304},
+    {lat: 48.433290692867246, lng: -123.41854441981755},
+    {lat: 48.433290692867246, lng: -123.4200062680723},
+    {lat: 48.433337776538814, lng: -123.42006303887831},
+    {lat: 48.43435477319516, lng: -123.4199069191618},
+    {lat: 48.43459930069949, lng: -123.41968549014572}
+  ]
+}
+
 function MapWindow(props) {
   const useKey = props.devMode ? devApiKey : apiKey
+  const [cutPolygons, setCutPolygons] = useState([]);
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: useKey
@@ -59,6 +70,30 @@ function MapWindow(props) {
     setMap(null)
   }, [])
 
+  const formatCutList = (cutArr) => {
+    // console.log('loaded');
+    console.log(cutArr);
+    const formattedCuts = cutArr.map((cutObj, x) => {
+      console.log('-----------',cutObj.polygonCoords);
+      return (
+        <Polygon 
+          key={`polygon-${x}_${cutObj.locationName}`}
+          onLoad={loadPolygon}
+          paths={cutObj.polygonCoords}
+          options={options}
+        />
+      )
+    })
+    console.log(formattedCuts)
+    return formattedCuts;
+    setCutPolygons(formattedCuts)
+  }
+
+  useEffect(() => {
+    const formattedCutElements = formatCutList(props.cutList);
+    setCutPolygons(formattedCutElements);
+  }, [])
+
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
@@ -67,15 +102,16 @@ function MapWindow(props) {
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
-        {props.cutList.map(outline => {
+        {cutPolygons}
+        {/* {props.cutList.map(outline => {
           return (
             <Polygon
               onLoad={loadPolygon}
-              paths={outline}
+              paths={cutPolygons}
               options={options}
             />
           )
-        })}
+        })} */}
         {/* <Polygon
         onLoad={loadPolygon}
         paths={nadenBaseballField}
